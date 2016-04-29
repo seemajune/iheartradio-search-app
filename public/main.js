bc052faf945ed8da91f5f1b1ef09e9838bd9ab96
seemajune <seemashariat@gmail.com>
2016-04-29 10:48:41 -0400

    saves a max of 6 items to scope.data

--- public/main.js
+++ public/main.js
@@ -1,13 +1,18 @@
 var radioSearch = angular.module('radioSearch', []);
 
 function mainController($scope, $http) {
-    $scope.formData = {};
+    $scope.formData = {},
+    $scope.data = [];
 
     $scope.createSearch = function() {
         $http.get('search/' + $scope.formData.artist)
         .success(function(data) {
-            $scope.data = JSON.parse(data.body);
-            console.log($scope.data); 
+            $scope.formData = {};
+            $scope.data = [];
+            $scope.tempArtists = JSON.parse(data.body);
+        
+            $scope.tempArtists.artists.map(function(info, artist){ return($scope.data.length < 6 ? $scope.data[artist] = info : $scope.data);});
+            console.log($scope.data);
         })
         .error(function(data) {
             console.log('Error: ', data);
--- server.js
+++ server.js
@@ -1,29 +1,24 @@
     var express  = require('express');
     var app      = express();                                     
-    var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+    var bodyParser = require('body-parser');    
     var request = require('request-json');
     var client = request.createClient('http://localhost:8080/');
     client.headers['Accept'] = 'application/json';
 
-    // configuration =================
+    app.use(express.static(__dirname + '/public'));                 
+    app.use(bodyParser.urlencoded({'extended':'true'}));            
+    app.use(bodyParser.json());                                     
+    app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
 
-    app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
-    app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
-    app.use(bodyParser.json());                                     // parse application/json
-    app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-
-    // Generic error handler used by all endpoints.
     function handleError(res, reason, message, code) {
       console.log("ERROR: " + reason);
       res.status(code || 500).json({"error": message});
     }
 
     app.get("/search/:term", function(req, res) {
-        console.log('req.params = ', req.params);
         var term = req.params.term;
         var staticQueryString = '&queryTrack=false&queryBundle=false&queryArtist=true&queryStation=false&queryFeatureStation=false&queryTalkShow=false&queryTalkTheme=false&queryKeyword=false&countryCode=US';
         client.get('http://api-3283.iheart.com/api/v1/catalog/searchAll?keywords=' + term + staticQueryString, function(err, data, body) {
-            console.log(data);
             if(err) {
                 handleError(res, err.message, "Failed to get catalog.");
             }
@@ -32,10 +27,9 @@
        
     });
 
-    // listen (start app with node server.js) ======================================
     app.listen(8080);
     console.log("App listening on port 8080");
 
     app.get('/', function (req, res) {
-     res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+     res.sendFile(__dirname + '/public/index.html'); 
     });
