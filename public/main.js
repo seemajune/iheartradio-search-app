dcb36e8c64711524fcfdeca8567afa70db862ecc
seemajune <seemashariat@gmail.com>
2016-04-29 15:06:16 -0400

    removes console errors by replacing img src with ng-src

--- public/main.js
+++ public/main.js
@@ -2,7 +2,6 @@ var radioSearch = angular.module('radioSearch', []);
 
 function mainController($scope, $http) {
     $scope.formData = {},
-    $scope.data = [];
 
     $scope.createSearch = function() {
         $http.get('search/' + $scope.formData.artist)
@@ -14,6 +13,7 @@ function mainController($scope, $http) {
             $scope.tempArtists.artists.map(function(obj, index, arr){ 
                 return( index < 6 ?  $scope.data.push({"image_url": 'http://iscale.iheart.com/catalog/artist/' + obj.artistId + '?ops=fit(250,0)', "name" : obj.artistName}) : $scope.data);
             });
+            console.log($scope.data);
            
         })
         .error(function(data) {
